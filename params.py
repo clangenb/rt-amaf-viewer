75d66e07a23419a6e35fbc1f8b983d41e3c6fb70
clangenbacher <christian.langenbacher@scs.ch>
2019-11-02 14:41:32 +0100

    moved smileconfig to smile/config, renamed smile_extenstion -> smile

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 SMILExtract = '../opensmile-2.3.0/inst/bin/SMILExtract'
-smile_config = 'smileconfig/live_ComParE_2016_reduced.conf'
+smile_config = 'smile/config/live_ComParE_2016_reduced.conf'
 
 config_str_a = 'run2_A_size300_step100_bs16_sl10_nl2_ss80_no10_std_0.9'
 config_str_v = 'V_size300_step100_bs16_sl30_nl2_ss80_no10'
