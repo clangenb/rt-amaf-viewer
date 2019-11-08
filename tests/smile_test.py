724a41053a5b31c5f3493c02bb6e1eb5f3026802
clangenbacher <christian.langenbacher@scs.ch>
2019-11-08 12:44:43 +0100

    moved/renamed tets files

--- tests/smile_test.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-smile_extract = subprocess.Popen(['SMILExtract', '-C', 'smileconfig/live_ComParE_2016_recuced.conf'])
-
--- tests/test_colorpalettes.py
+++
@@ -1,9 +0,0 @@
-import plotters.myplot as mp
-import matplotlib.pyplot as plt
-
-#
-# cp = mp.ColorPalette()
-# plt.show()
-#
-sn = mp.AnimationSnapShot()
-plt.show()
