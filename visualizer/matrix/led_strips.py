e70fecd4caa3fe6c2ac82da85342254dc4ca9d25
clangenbacher <christian.langenbacher@scs.ch>
2019-11-20 16:46:35 +0100

    [visualizer/matrix/led_strips] Added LazyTcpStrip

--- visualizer/matrix/led_strips.py
+++ visualizer/matrix/led_strips.py
@@ -2,9 +2,25 @@ class TcpStrip:
     def __init__(self, protocol):
         self._proto = protocol
 
-    def show(self):
-        self._proto.sendLine("show".encode("ascii"))
-
     def setPixelColor(self, i, color):
         # print("Pixel no{}, Pixel Color {}".format(i, color))
         self._proto.sendLine("{},{}".format(i, color).encode("ascii"))
+
+    def show(self):
+        self._proto.sendLine("show".encode("ascii"))
+
+
+class LazyTcpStrip:
+    def __init__(self, protocol):
+        self._proto = protocol
+        self._updated_pixels = {}
+
+    def setPixelColor(self, i, color):
+        self._updated_pixels[i] = color
+
+    def show(self):
+        leds_to_update = ""
+        for i, color in self._updated_pixels.items():
+            leds_to_update += "{},{}".format(i, color).encode("ascii")
+
+        self._proto.sendLine(leds_to_update)
